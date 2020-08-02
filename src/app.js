7adf654e0272210357f48f9c00afe181ff0cfb23
Jay Desai <jaydesai571@gmail.com>
2020-08-02 11:30:18 -0400

    NPM install added using install-import-use method and sample example

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-// import './utils.js';
-// import { sqaure, add } from './utils.js';
-import { isAdult, canDrink} from './utils.js';
+// // import './utils.js';
+// // import sub, { sqaure, add } from './utils.js';
+// import isSenior, { isAdult, canDrink} from './utils.js';
 
-console.log('App is running');
-console.log(isAdult(18));
-console.log(canDrink(19));
+// console.log('App is running');
+// console.log(isAdult(18));
+// console.log(canDrink(19));
+// console.log(isSenior(65));
+// // console.log(sqaure(4));
+// // console.log(add(100,2000));
+// // console.log(sub(100,2000));
 
-// console.log(sqaure(4));
-// console.log(add(100,2000));
+//Email validation using 3 party import Validator
+// import validator from 'validator';
+// console.log(validator.isEmail('test@gmail.com'));
 
+
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const template = <p>This is JSX from Webpack</p>;
+ReactDom.render(template, document.getElementById('app'));
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,12 @@
 console.log('Utils.js is running');
 
 // const sqaure = (x) => x*x;
-
 // const add = (a,b) => a+b;
-
-// export { sqaure, add };
+// const sub = (a,b) => a-b;
+// export { sqaure, add, sub as default };
 
 export const isAdult = (x) => x >= 18;
 
 export const canDrink = (x) => x >= 19;
+
+export  default (x) => x >= 65; 
