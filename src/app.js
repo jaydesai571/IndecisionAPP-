80e08c49cce69df87d265900d18b384f7bc71e4d
Jay Desai <jaydesai571@gmail.com>
2020-08-03 00:44:30 -0400

    All modelues are separate in each files making APP.js small

--- src/app.js
+++ src/app.js
@@ -1,23 +1,5 @@
-// // import './utils.js';
-// // import sub, { sqaure, add } from './utils.js';
-// import isSenior, { isAdult, canDrink} from './utils.js';
-
-// console.log('App is running');
-// console.log(isAdult(18));
-// console.log(canDrink(19));
-// console.log(isSenior(65));
-// // console.log(sqaure(4));
-// // console.log(add(100,2000));
-// // console.log(sub(100,2000));
-
-//Email validation using 3 party import Validator
-// import validator from 'validator';
-// console.log(validator.isEmail('test@gmail.com'));
-
-
-
 import React from 'react';
 import ReactDom from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-const template = <p>This is JSX from Webpack</p>;
-ReactDom.render(template, document.getElementById('app'));
+ReactDom.render(<IndecisionApp options={['Devils den', 'second District']} />,document.getElementById('app'))
