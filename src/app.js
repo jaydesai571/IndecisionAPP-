1462a2330159278379770c75bca40d5636d14b6c
Jay Desai <jaydesai571@gmail.com>
2020-08-04 23:38:43 -0400

    Complete dev server and starting off with 3 party tools like REACT MODEL for designing.

--- src/app.js
+++ src/app.js
@@ -2,4 +2,21 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDom.render(<IndecisionApp options={['Devils den', 'second District']} />,document.getElementById('app'))
+// const Layout = (props) => {
+//     return (
+//         <div>
+//             <p>Header</p>
+//             {props.content}
+//             <p>Footer</p>
+//         </div>
+//     );
+// };
+
+// const template = (
+//     <div>
+//         <h1>Page Title</h1>
+//         <p>This is my pages</p>
+//     </div>
+// );
+// ReactDom.render(<Layout content= {template} />,document.getElementById('app'));
+ReactDom.render(<IndecisionApp options={['Devils den', 'second District']} />,document.getElementById('app'));
