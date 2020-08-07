a4510e7ab804bcf68ac965e624337046b943b45f
Jay Desai <jaydesai571@gmail.com>
2020-08-07 01:37:57 -0400

    SCSS thorugh Webpack add and tested

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
 
 ReactDom.render(<IndecisionApp options={['Devils den', 'second District']} />,document.getElementById('app'));
 
